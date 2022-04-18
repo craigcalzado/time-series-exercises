5730c66b14a78074c40c457e204fa98a4b47df7d
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-04-18 15:59:30 -0500

    added split function

--- timeseries_prepare.py
+++ timeseries_prepare.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def trinity_prep(df):
@@ -23,3 +24,8 @@ def opsd_prep(df):
     df['Solar'].fillna(0, inplace=True)
     df['Wind+Solar'] = df['Wind'] + df['Solar']
     return df
+#-------------------------------------------------------------------------------
+def split_dataframe(df):
+   train, test = train_test_split(df, test_size=0.2, random_state=789, stratify=df['index'])
+   train, validate = train_test_split(train, test_size=0.3, random_state=789, stratify=train['index'])
+   return train, validate, test 
